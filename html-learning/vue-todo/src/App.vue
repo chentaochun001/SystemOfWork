b143a97ff6d0ecfd7212683653d2a03f166ea1e2
XbaiAmy <1621810784@qq.com>
2018-01-03 22:49:04 +0800

    有关vue的组件

---
+++ html-learning/vue-todo/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="app" class="container">
+        <div class="todo-wrapper">
+            <todo-header></todo-header>
+            <div class="todo-content">
+                <todo-report></todo-report>
+                <todo-list></todo-list>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    // 引用组件，使用第三方组件
+    import TodoHeader from './components/TodoHeader.vue';
+    import TodoReport from './components/TodoReport.vue';
+    import TodoList from './components/TodoList.vue';
+
+    export default {
+        components: {
+            // "todo-header": Header,
+            // "todo-report":  TodoReport,
+            // "todo-list": TodoList
+
+            // 简写
+            TodoHeader,
+            TodoReport,
+            TodoList
+        }
+    }
+</script>
+<style>
+</style>
---
+++ html-learning/vue-todo/src/components/TodoReport.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        Report
+    </div>
+</template>
+
+<script>
+    export default {}
+</script>
+
+<style>
+</style>
