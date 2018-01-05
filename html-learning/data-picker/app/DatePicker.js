a1d78ec7641f92baca214b55f3cb1ba207d7bc35
XbaiAmy <1621810784@qq.com>
2018-01-05 23:09:35 +0800

    引入了日历显示组件，增加了显示日历的功能

--- html-learning/data-picker/app/DatePicker.js
+++ html-learning/data-picker/app/DatePicker.js
@@ -1,5 +1,26 @@
+import $ from "jquery";
+// webpack 不会重复打包 jquery被n个文件引用  但最后只打包一个jquery 根据引用顺序进行打包
+import template from "../template/picker.hbs";
+
 export default class DatePicker {
     constructor (el, options) {
         console.log('DatePicker');
+        this.createElement(options);
+        this.delegateEvent(options);
+    }
+    createElement(options) {
+        let o = {
+            static: true,
+            scattered: true,
+            confirm: true
+        }
+        let item = $(template(o));
+        item.appendTo(document.body);
+        setTimeout(() => {
+            item.removeClass('out')
+        }, 10);
+    }
+    delegateEvent(options) {
+
     }
 }
