84ef84f9076054bae6b44f6ad1debd596ffc77d3
XbaiAmy <1621810784@example.com>
2018-01-08 23:04:34 +0800

    评论列表组件

---
+++ vue-comment/src/components/CommentList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="commentList">
+      <comment-item v-for="item in List" :comment="item" :key="item.id"></comment-item>
+      <div v-if="List.length == 0" class="empty-msg">当前没有评论</div>
+  </div>
+</template>
+<script>
+import CommentItem from './CommentItem.vue';
+export default {
+    name: 'commentList',
+    props: ['List'],
+    components: {
+        'comment-item': CommentItem,
+    }
+}
+</script>
+<style>
+  .commentList{
+  background: #eee;
+  color: #000;
+  font-size: 15px;
+  margin:10px;
+  width: 300px;
+  border:1px solid #eee;
+  margin:10px auto;
+}
+
+</style>
