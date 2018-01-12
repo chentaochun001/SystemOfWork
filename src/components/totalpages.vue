603c0bcf021ba2756f516e34fa8cfd1341a365b3
XbaiAmy <1621810784@example.com>
2018-01-12 23:12:53 +0800

    有关vue中element-ui 开发后台的学习

---
+++ src/components/totalpages.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>totalpages</div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+<style>
+
+</style>
---
+++ src/pages/activePublic/step4.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>Step4</div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style>
+
+</style>
