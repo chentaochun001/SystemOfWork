a7b7925d6caef8c402998be29b99b5a98d88d9f6
XbaiAmy <1621810784@qq.com>
2018-03-21 22:02:33 +0800

    有关typescript的学习

---
+++ react/qs_react/react-typescript/src/index.tsx
@@ -0,0 +1,10 @@
+// console.log('Hello from ts');
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+// import { HelloComponent } from './hello'
+import { AppRouter } from './router'
+
+ReactDOM.render(
+    <AppRouter />,
+    document.getElementById('root')
+)
---
+++ react/qs_react/user-dashboard/src/routes/Users.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { connect } from 'dva';
+import styles from './Users.css';
+import UsersComponent from '../components/Users/Users';
+
+function Users() {
+  return (
+    <div className={styles.normal}>
+      <UsersComponent />
+    </div>
+  );
+}
+
+function mapStateToProps() {
+  return {};
+}
+
+export default connect(mapStateToProps)(Users);
